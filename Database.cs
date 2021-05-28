841db9afbb2f671463280f555c0a4ab7bf07ab77
unknown <danil.sidorov16@gmail.com>
2021-05-28 21:30:23 +0300

    Database class and Drawing packet

---
+++ Database.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Windows;
+
+namespace Works
+{
+    public static class Database
+    {
+        private static SqlConnection sqlConnection =
+            new SqlConnection(@"Provider=Microsoft.ACE.OLEDB.12.0; Persist Security Info=False; Data Source=|DataDirectory|\dbo.mdb; ");
+
+        public static DataTable SendQuery(string query)
+        {
+            DataTable resultat = new DataTable();
+            try
+            {
+                sqlConnection.Open();
+                SqlCommand command = new SqlCommand(query, sqlConnection);
+
+                SqlDataAdapter chitat1 = new SqlDataAdapter(command);
+
+                chitat1.Fill(resultat);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Не удалось выполнить действие: {ex.Message}");
+            }
+            sqlConnection.Close();
+
+            return resultat;
+        }
+
+        public static List<Service> GetServices()
+        {
+            List<Service> list = new List<Service>();
+            DataTable table = SendQuery($"Select * from dbo.Service");
+            foreach (DataRow row in table.Rows)
+            {
+                list.Append(new Service
+                {
+                    Id = (int)row.ItemArray[0],
+                    Title = row.ItemArray[1].ToString(),
+                    Cost = (int)row.ItemArray[2],
+                    Time = (int)row.ItemArray[3],
+                    Sale = (float)row.ItemArray[5],
+                    Image = Image.FromFile(row.ItemArray[6].ToString()),
+                });
+            }
+            return list;
+        }
+
+        //public static void AddObject(object element, DataType type)
+        //    => SendQuery($"Insert into dbo.{DataTypes[type]} values {element}");
+
+        //public static void DeleteObject(int id, DataType type)
+        //    => SendQuery($"Delete from dbo.{DataTypes[type]} where id = {id}");
+    }
+}
--- Service.cs
+++ Service.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
-
+﻿using System.Drawing;
 namespace Works
 {
     public class Service
     {
-        public int Id { get; private set; }
-        public string Title { get; private set; }
-        public int Cost { get; private set; }
-        public DateTime Time { get; private set; }
-        public BitmapImage Image { get; private set; }
-        public float Sale { get; private set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Cost { get; set; }
+        public int Time { get; set; }
+        public Image Image { get; set; }
+        public float Sale { get; set; }
     }
 }
