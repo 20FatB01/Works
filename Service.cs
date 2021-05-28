cb02ebde44715c89860fe73949a46cd9f911036f
unknown <danil.sidorov16@gmail.com>
2021-05-28 17:28:59 +0300

    Service class

---
+++ Service.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Works
+{
+    public class Service
+    {
+        public int Id { get; private set; }
+        public string Title { get; private set; }
+        public int Cost { get; private set; }
+        public DateTime Time { get; private set; }
+        public BitmapImage Image { get; private set; }
+        public float Sale { get; private set; }
+    }
+}
